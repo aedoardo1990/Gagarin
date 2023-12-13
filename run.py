303413d6e4e59d76604e552e567dc67518dcbfe3
aedoardo1990 <>
2023-12-13 12:40:38 +0000

    try to add welcome image with PIL library

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import PIL.Image
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -40,5 +41,53 @@ while True:
         print("Blank username is invalid. Please enter letters, numbers or combination of both as username.\n")
         continue 
     else:
-        print("приве́т," + username + "!")
+        print("привет," + username + "!")
         break
+
+# ascii characters used to build the output text
+ASCII_CHARS = ["@","#","S","%","?","*","+",";",":",",","."]
+
+#resize image according to a new width
+def resize_image(image, new_width=100):
+    width, height = image.size
+    ratio = height / width / 1.65
+    new_height = int(new_width * ratio)
+    resized_image = image.resize((new_width, new_height))
+    return(resized_image)
+
+# convert each pixel to grayscale 
+def grayify(image):
+    grayscale_image = image.convert("L")
+    return(grayscale_image)
+
+# convert pixels to a string of ASCII characters
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
+    return(characters)
+
+def main(new_width=10):
+    # attempt to open image from user-input
+    path = input("Enter a valid pathname to an image:\n")
+    try:
+        image = PIL.Image.open(path)
+    except:
+        print(path, "is not a valid pathname to an image.")
+    
+    # convert image to ASCII
+    new_image_data = pixels_to_ascii(grayify(resize_image(image)))
+
+    # format
+    pixel_count = len(new_image_data)
+    ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+
+    # print result 
+    print(ascii_image)
+
+    #save result to "ascii_image.txt"
+    with open("ascii_image.txt", "w") as f:
+        f.write(ascii_image)
+
+
+
+main()
