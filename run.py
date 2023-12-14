eee0b39e94acb7a0ba6c34eb311c8906dce37c3f
aedoardo1990 <>
2023-12-14 09:25:17 +0000

    add dictionaries and lists to map questions, answers, soviet stories, hints

--- run.py
+++ run.py
@@ -30,6 +30,34 @@ data4 = soviet_story.get_all_values()
 player_scores = SHEET.worksheet('player_scores')
 data5 = player_scores.get_all_values()
 
+# map questions from Google Sheet - dictionary
+questions = {
+    question[0][0]: 'A',
+    question[1][0]: 'B',
+    question[2][0]: 'C'
+}
+
+# map answers from Google Sheet - list of lists
+answers = [
+    [answer[0][0], answer[0][1], answer[0][2]],
+    [answer[1][0], answer[1][1], answer[1][2]],
+    [answer[2][0], answer[2][1], answer[2][2]]
+]
+
+# map hints from Google Sheet - dictionary
+hints = {
+    hint[0][0],
+    hint[1][0],
+    hint[2][0]
+}
+
+# map soviet story from Google Sheet - dictionary
+soviet_stories = hints = {
+    story[0][0],
+    story[1][0],
+    story[2][0]
+}
+
 # Welcome message
 print("Hello and welcome to Soviet Union!\n")
 print("You are right, USSR is dead but it still lives in the memory of its")
@@ -43,7 +71,7 @@ while True:
         print(f"or combination of both as username.\n{Fore.WHITE}")
         continue
     else:
-        print("\nпривет," + username + "!")
+        print("\nпривет, товарищ " + username + "!")
         break
 
 print("\nDo you want to read the game rules? Y/N \n")
@@ -51,7 +79,11 @@ print("\nDo you want to read the game rules? Y/N \n")
 while True:
     rules = input("")
     if rules == "Y":
-        print("\nHere are the instructions")
+        print("\nHere are the instructions:\n")
+        print("★ Choose the correct answer among the options A), B), C)")
+        print("★ If the answer is correct you will receive 10 points")
+        print("★ If the answer is incorrect you will receive 0 points")
+        print("★ If you want a Hint before choosing the answer, pls enter H\n")
         break
     elif rules == "N":
         print("\nLet's play!")
@@ -59,3 +91,8 @@ while True:
     else:
         print(f"\n{Fore.RED}Choice is invalid, please enter Y or N{Fore.WHITE}\n")
         continue
+
+def start_quiz():
+    """
+    Function starts the quiz
+    """
