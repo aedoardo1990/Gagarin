10c163ad468e2cf12969ebcb8cd8df4057838a54
aedoardo1990 <>
2023-12-21 01:36:58 +0000

    try to fix issue with leaderboard table

--- run.py
+++ run.py
@@ -317,8 +317,7 @@ def leaderboard():
         return str(dat[1])
 
     score.sort(key=size, reverse=True)
-    # credits to https://stackoverflow.com/questions/76734963/colorama-not
-    # -working-with-tabulate-to-display-colored-output-in-python
+    
     table = tabulate(score[0:11], tablefmt='fancy_grid', stralign='center')
     colored_table = red(table)
 
